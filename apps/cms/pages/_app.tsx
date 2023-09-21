7b6ee5da091f5b74983d95202764de30016c9cac
Andri Purnomo <andri.adrp@gmail.com>
2023-09-21 19:15:47 +0700

    chore: initial setup

--- apps/cms/pages/_app.tsx
+++ apps/cms/pages/_app.tsx
@@ -1,17 +1,11 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
-import Head from 'next/head';
-import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <title>Welcome to cms!</title>
-      </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
-    </>
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
   );
 }
 
