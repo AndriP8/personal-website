044411c413313e1083cd0fadc1d8746b9b4cba55
Andri Purnomo <andri.adrp@gmail.com>
2023-10-15 10:56:24 +0700

    feat(cms): integrate sign up and sign in page

--- apps/cms/pages/sign-in.tsx
+++ apps/cms/pages/sign-in.tsx
@@ -1,7 +1,6 @@
 import {
   Box,
   Button,
-  Checkbox,
   Container,
   FormControl,
   FormLabel,
@@ -9,11 +8,58 @@ import {
   Input,
   Stack,
   Text,
+  useToast,
 } from '@chakra-ui/react';
 import { PasswordField } from '@personal-website/cms/component';
+import axios, { AxiosError } from 'axios';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+
+type SignInResponse = {
+  token: string;
+};
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const SignIn = () => {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const toast = useToast();
+  const router = useRouter();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    axios
+      .post<SignInResponse>(`${BASE_URL}users/login`, {
+        email,
+        password,
+      })
+      .then((res) => {
+        localStorage.setItem('token', res.data.token);
+        toast({
+          title: 'Sign in successfully',
+          status: 'success',
+          duration: 3000,
+          isClosable: true,
+          position: 'top-right',
+        });
+        router.push('/');
+      })
+      .catch((error: unknown) => {
+        if (error instanceof AxiosError) {
+          toast({
+            title: 'Sign in error',
+            description: error.response?.data.errors,
+            status: 'error',
+            duration: 3000,
+            isClosable: true,
+            position: 'top-right',
+          });
+        }
+      });
+  };
   return (
     <Box backgroundColor="gray.100" minHeight="100vh" height="full">
       <Container
@@ -33,30 +79,39 @@ const SignIn = () => {
               </Text>
             </Stack>
           </Stack>
-          <Box
-            py={{ base: '4', sm: '8' }}
-            px={{ base: '4', sm: '10' }}
-            mx={{ base: '2', sm: '0' }}
-            bg="white"
-            boxShadow={{ base: 'none', sm: 'md' }}
-            borderRadius={{ base: 'md', sm: 'xl' }}
-          >
-            <Stack spacing="6">
-              <Stack spacing="5">
-                <FormControl>
-                  <FormLabel htmlFor="email">Email</FormLabel>
-                  <Input id="email" type="email" />
-                </FormControl>
-                <PasswordField />
+          <form onSubmit={handleSubmit}>
+            <Box
+              py={{ base: '4', sm: '8' }}
+              px={{ base: '4', sm: '10' }}
+              mx={{ base: '2', sm: '0' }}
+              bg="white"
+              boxShadow={{ base: 'none', sm: 'md' }}
+              borderRadius={{ base: 'md', sm: 'xl' }}
+            >
+              <Stack spacing="6">
+                <Stack spacing="5">
+                  <FormControl>
+                    <FormLabel htmlFor="email">Email</FormLabel>
+                    <Input
+                      id="email"
+                      type="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </FormControl>
+                  <PasswordField
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </Stack>
+                <Box>
+                  <Button type="submit" width="full">
+                    Sign In
+                  </Button>
+                </Box>
               </Stack>
-              <Box>
-                <Checkbox defaultChecked>Remember me</Checkbox>
-              </Box>
-              <Box>
-                <Button width="full">Sign In</Button>
-              </Box>
-            </Stack>
-          </Box>
+            </Box>
+          </form>
         </Stack>
       </Container>
     </Box>
--- apps/cms/pages/sign-up.tsx
+++ apps/cms/pages/sign-up.tsx
@@ -8,11 +8,57 @@ import {
   Input,
   Stack,
   Text,
+  useToast,
 } from '@chakra-ui/react';
 import { PasswordField } from '@personal-website/cms/component';
+import { User } from '@prisma/client';
+import axios, { AxiosError } from 'axios';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+
+type SignUpResponse = Omit<User, 'password'>;
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const SignUp = () => {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const toast = useToast();
+  const router = useRouter();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    axios
+      .post<SignUpResponse>(`${BASE_URL}users/login`, {
+        email,
+        password,
+      })
+      .then(() => {
+        toast({
+          title: 'Sign up successfully',
+          status: 'success',
+          duration: 3000,
+          isClosable: true,
+          position: 'top-right',
+        });
+        router.push('/sign-in');
+      })
+      .catch((error: unknown) => {
+        if (error instanceof AxiosError) {
+          toast({
+            title: 'Sign up error',
+            description: error.response?.data.errors,
+            status: 'error',
+            duration: 3000,
+            isClosable: true,
+            position: 'top-right',
+          });
+        }
+      });
+  };
+
   return (
     <Box backgroundColor="gray.100" minHeight="100vh" height="full">
       <Container
@@ -31,27 +77,39 @@ const SignUp = () => {
               </Text>
             </Stack>
           </Stack>
-          <Box
-            py={{ base: '4', sm: '8' }}
-            px={{ base: '4', sm: '10' }}
-            mx={{ base: '2', sm: '0' }}
-            bg="white"
-            boxShadow={{ base: 'none', sm: 'md' }}
-            borderRadius={{ base: 'md', sm: 'xl' }}
-          >
-            <Stack spacing="6">
-              <Stack spacing="5">
-                <FormControl>
-                  <FormLabel htmlFor="email">Email</FormLabel>
-                  <Input id="email" type="email" />
-                </FormControl>
-                <PasswordField />
+          <form onSubmit={handleSubmit}>
+            <Box
+              py={{ base: '4', sm: '8' }}
+              px={{ base: '4', sm: '10' }}
+              mx={{ base: '2', sm: '0' }}
+              bg="white"
+              boxShadow={{ base: 'none', sm: 'md' }}
+              borderRadius={{ base: 'md', sm: 'xl' }}
+            >
+              <Stack spacing="6">
+                <Stack spacing="5">
+                  <FormControl>
+                    <FormLabel htmlFor="email">Email</FormLabel>
+                    <Input
+                      id="email"
+                      type="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </FormControl>
+                  <PasswordField
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </Stack>
+                <Box>
+                  <Button type="submit" width="full">
+                    Sign Up
+                  </Button>
+                </Box>
               </Stack>
-              <Box>
-                <Button width="full">Sign Up</Button>
-              </Box>
-            </Stack>
-          </Box>
+            </Box>
+          </form>
         </Stack>
       </Container>
     </Box>
