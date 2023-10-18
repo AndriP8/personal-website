dc5f9852c24e1c9fc23a11080e7e10f71d6f8581
Andri Purnomo <andri.adrp@gmail.com>
2023-10-18 16:42:42 +0700

    test(cms): fix test temporary

--- libs/cms/component/src/layout/Layout.spec.tsx
+++ libs/cms/component/src/layout/Layout.spec.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 
 import Layout from './Layout';
-
+jest.mock('next/router', () => jest.requireActual('next-router-mock'));
 describe('Layout', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<Layout />);
--- libs/cms/component/src/sidebar/Sidebar.spec.tsx
+++ libs/cms/component/src/sidebar/Sidebar.spec.tsx
@@ -2,6 +2,7 @@ import { render } from '@testing-library/react';
 
 import Sidebar from './Sidebar';
 
+jest.mock('next/router', () => jest.requireActual('next-router-mock'));
 describe('Sidebar', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<Sidebar />);
