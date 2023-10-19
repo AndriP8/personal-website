01ee411bd4628205a2d7f30131cd404b169d8c98
Andri Purnomo <andri.adrp@gmail.com>
2023-10-19 15:13:00 +0700

    chore(web-app): init nextjs app

---
+++ apps/web-app/pages/_app.tsx
@@ -0,0 +1,18 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+function CustomApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Welcome to web-app!</title>
+      </Head>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
+}
+
+export default CustomApp;
---
+++ apps/web-app/pages/index.tsx
@@ -0,0 +1,7 @@
+import { Heading } from '@chakra-ui/react';
+
+export function Index() {
+  return <Heading color="red.900">Hello web app</Heading>;
+}
+
+export default Index;
